443774736ce1934feb38fa684e5829525c4ada22
kian-g <kian@reconventures.com>
2023-11-10 14:26:11 -0800

    better code

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const config = require("../Config/config.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,13 +12,13 @@ module.exports = {
 
     async execute(interaction) {
         const suggestion = interaction.options.getString('message');
-        const suggestionChannelId = '1172658205562769548'; // Channel ID where suggestions will be sent
+        const suggestionChannelId = config.suggestionChannel; // Channel ID where suggestions will be sent
 
         try {
             // Fetch the suggestion channel
             const suggestionChannel = await interaction.client.channels.fetch(suggestionChannelId);
             if (!suggestionChannel) {
-                await interaction.reply({ content: 'Suggestion channel not found. Please join the server in person to give feedback! https://discord.gg/AA2HM5Pw4H', ephemeral: true });
+                await interaction.reply({ content: `Suggestion channel not found. Please join the server in person to give feedback! ${config.supportServerLink}`, ephemeral: true });
                 return;
             }
 
@@ -28,7 +29,7 @@ module.exports = {
             await interaction.reply({ content: 'Your suggestion has been submitted successfully!', ephemeral: true });
         } catch (error) {
             console.error('Failed to submit suggestion:', error);
-            await interaction.reply({ content: 'There was an error while submitting your suggestion. Please join the server in person to give feedback! https://discord.gg/AA2HM5Pw4H', ephemeral: true });
+            await interaction.reply({ content: `There was an error while submitting your suggestion. Please join the server in person to give feedback! ${config.supportServerLink}`, ephemeral: true });
         }
     },
 };
